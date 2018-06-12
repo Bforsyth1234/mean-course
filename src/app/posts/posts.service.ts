967d043db0b3022286b170750b51848a6b22b902
Brooks Forsyth <brooks.forsyth@gmail.com>
2018-06-12 08:18:27 -0400

    Navigate home when done editing a task

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject, identity } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,7 +12,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
@@ -48,6 +49,7 @@ export class PostsService {
       post.id = id;
       this.posts.push(post);
       this.postUpdated.next([...this.posts]);
+      this.router.navigate(['/']);
     });
   }
 
@@ -60,6 +62,7 @@ export class PostsService {
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
 
   }
